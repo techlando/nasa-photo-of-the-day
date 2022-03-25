23fc415ab5fee9238d08b277305b42dd4f2cccba
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-03-24 20:41:22 -0500

    styled components mvp

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,16 +1,40 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
+from {
+    transform: rotate(0deg);
+  }
+  to {
+    
+    transform: rotate(360deg);
+  }
+`;
+
+const NasaWrapper = styled.div`
+    background-color: #ADD8E6;
+    
+    animation: ${kf} 2s linear 1;
+    
+`
+const Newh1 = styled.h1`
+&:hover {
+    background-color: red;
+    
+}
+`
 
 
 
     
 const NasaPhoto = (props) => {
     return (
-        <div className='nasa-photo-wrapper'>
-            <h3>{props.photo.title}</h3>
+        <NasaWrapper className='nasa-photo-wrapper'>
+            <Newh1>{props.photo.title}</Newh1>
             <p>{props.photo.date}</p>
             <img src={props.photo.hdurl}/>
         <p className="explanation">{props.photo.explanation}</p>
-        </div>
+        </NasaWrapper>
     )
 }
 export default NasaPhoto;
